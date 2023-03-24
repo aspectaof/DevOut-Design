aeed829c8bb24acf7d086aefbfdd72a03d143b54
Aspecta Of <119008242+aspectaof@users.noreply.github.com>
2023-03-24 17:04:41 -0300

    Atualizar o DevStart.sh

--- DevStart.sh
+++ DevStart.sh
@@ -18,7 +18,7 @@ pkg install ruby -y
 gem install lolcat 
 figlet -f big Wait | lolcat
 git clone https://github.com/BDhackers009/simple-edit
-sed 's+THBD+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
-sed 's+THBD+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
+sed 's+DevOut+'$user'+g' simple-edit/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+DevOuf+'$user'+g' simple-edit/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
 rm -rf simple-edit
 figlet -f big DevOut | lolcat
